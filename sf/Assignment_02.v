9e3c79fe77a4fc31ea24c95e2649925098097b32
Hyunjin Jeong <das01063@snu.ac.kr>
2015-03-20 15:53:30 +0900

    Assignment02

--- sf/Assignment_02.v
+++ sf/Assignment_02.v
@@ -225,11 +225,6 @@ Definition swap_pair (p : natprod) : natprod :=
   | (x,y) => (y,x)
   end.
 
-
-
-
-
-
 (** **** Problem #9 : 1 star (snd_fst_is_swap) *)
 Theorem snd_fst_is_swap : forall (p : natprod),
   (snd p, fst p) = swap_pair p.
@@ -247,4 +242,3 @@ Proof.
   destruct p.
   reflexivity.
 Qed.
-
