faa31e747975a23a35d7aa81babd982d91a0bd9c
Hyunjin Jeong <das01063@snu.ac.kr>
2015-05-21 10:52:23 +0900

    16 update

--- sf/Assignment08_16.v
+++ sf/Assignment08_16.v
@@ -9,7 +9,7 @@ Proof.
   aexp_cases (induction a) Case; simpl;
   try reflexivity; try (rewrite IHa1; rewrite IHa2; reflexivity).
   destruct a1; try destruct n; destruct a2; try destruct n;
-  try simpl in *; try rewrite IHa2; try destruct n0; try simpl in *; try omega.
+  try rewrite IHa2; try destruct n0; try simpl in *; try omega.
 Qed.
 
 (*-- Check --*)
